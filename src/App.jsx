cd38fbe120f00a67b585cfc5f17235a7728cedba
mauto tenti <mauro.tenti@beije.it>
2022-06-01 15:33:25 +0200

    init - blank

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <p
+      style={{
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+      }}
+    >
+      :&#41;
+    </p>
+  );
+}
+
+export default App;
